96420bad93833f410def7996018b6b8d2c95d118
Moonlight <yuechuhaoxi020609@outlook.com>
2023-04-03 21:16:11 +0800

    first commit

---
+++ test/tools/checkWinTest.java
@@ -0,0 +1,17 @@
+package tools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class checkWinTest {
+    @Test
+    public void test1(){
+        StaticTool tool = new StaticTool();
+    }
+
+    @Test
+    public void test2(){
+        StaticTool tool = new StaticTool();
+    }
+}
---
+++ test/tools/computeBombTest.java
@@ -0,0 +1,17 @@
+package tools;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class computeBombTest {
+    @Test
+    public void test1(){
+        LayBomb tool = new LayBomb();
+    }
+
+    @Test
+    public void test2(){
+        LayBomb tool = new LayBomb();
+    }
+}
